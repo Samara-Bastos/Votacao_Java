250d79a60349a3a806a3f9214385af07affdba1d
Samara Bastos <samarabastos.dev@gmail.com>
2024-07-17 17:49:27 -0300

    Implementação  da migração para a criação das tabelas no banco de dados

---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Pauta (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255),
+    descricao TEXT,
+    categoria VARCHAR(30)
+);
+
+CREATE TABLE Sessao_Votacao (
+    id SERIAL PRIMARY KEY,
+    pauta_id INTEGER,
+    tempo_sessao TIMESTAMP,
+    situacao VARCHAR(20),
+    FOREIGN KEY (pauta_id) REFERENCES Pauta(id)
+);
+
+CREATE TABLE Usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    cpf VARCHAR(11) UNIQUE
+);
+
+CREATE TABLE Voto (
+    id SERIAL PRIMARY KEY,
+    tipo VARCHAR(3),
+    usuario_id INTEGER,
+    sessao_votacao_id INTEGER,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+    FOREIGN KEY (sessao_votacao_id) REFERENCES Sessao_Votacao(id)
+);
