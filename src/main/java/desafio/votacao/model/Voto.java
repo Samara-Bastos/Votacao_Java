b29fbc28f65023bdea68377adf06d06f03f66610
Samara Bastos <samarabastos.dev@gmail.com>
2024-07-17 17:48:03 -0300

    Criação das entidades principais

---
+++ src/main/java/desafio/votacao/model/Voto.java
@@ -0,0 +1,34 @@
+package desafio.votacao.model;
+
+import desafio.votacao.model.enums.TipoVoto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Voto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TipoVoto tipo;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "sessao_votacao_id")
+    private SessaoVotacao votacao;
+
+}
