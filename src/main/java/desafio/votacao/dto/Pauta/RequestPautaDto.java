c069a13d924443c55cc4606afa4bf7079453c414
Samara Bastos <samarabastos.dev@gmail.com>
2024-07-23 12:17:29 -0300

    Implementado a validação dos campos obrigatórios

--- src/main/java/desafio/votacao/dto/Pauta/RequestPautaDto.java
+++ src/main/java/desafio/votacao/dto/Pauta/RequestPautaDto.java
@@ -1,11 +1,19 @@
 package desafio.votacao.dto.Pauta;
 
 import desafio.votacao.enums.Categoria;
+import jakarta.validation.constraints.NotBlank;
 
 public record RequestPautaDto(
+
+    @NotBlank(message  = "O titulo não pode estar vazio")
     String titulo,
+
     String descricao,
+
+    @NotBlank(message  = "A categoria não pode estar vazia")
     Categoria categoria,
+
     Boolean ativaSessao,
+    
     int tempoSessao
 ) {}
--- src/main/java/desafio/votacao/dto/Voto/RequestVotoDto.java
+++ src/main/java/desafio/votacao/dto/Voto/RequestVotoDto.java
@@ -1,8 +1,12 @@
 package desafio.votacao.dto.Voto;
 
 import desafio.votacao.enums.TipoVoto;
+import jakarta.validation.constraints.NotBlank;
 
 public record RequestVotoDto(
+    @NotBlank(message = "O voto precisa ser preenchido")
     TipoVoto tipo,
+
+    @NotBlank(message = "O CPF precisa ser preenchido")
     String cpf
 ) {}
